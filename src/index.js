836b525e858db396009004b338838281da00c07b
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2019-05-26 00:26:30 +0700

    reformat

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from "./App";
 // import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
 
 // registerServiceWorker();
