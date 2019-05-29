93cc65f46e0965e5e3c65e2224d430f38d66c065
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2019-05-29 20:44:58 +0700

    update dependencies

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 Personal resume, blog and portfolio with ReactJS
 
 -   Eslint + Prettier Config
+-   Redux + Thunk + Reselect
 -   Husy + Lint-staged
 -   Test: Jest + Enzyme
 -   Craco (customized webpack, babel)
--   I18n
--   Routes + Loadable
+-   I18n integreted with Redux
+-   Routes with Loadable
 -   Utils: Authentication, localStorage, apiCaller
+-   Auto loading bar redux based
 
 -   Admin (Authentication => Create/Update/Delete Blog, Delete Comment)
 -   Comment
--- src/App.js
+++ src/App.js
@@ -10,34 +10,54 @@ import Resume from "./components/Resume";
 import Intro from "./components/Intro";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <BackTop>
-            <Icon type="up-circle" />
-          </BackTop>
-          <Row>
-            <Col lg={12}>
-              <Intro />
-            </Col>
-            <Col lg={12}>
-              <div>
-                <Switch>
-                  <Route exact path="/login" component={Login} />
-                  <Route exact path="/blogs/:page" component={ListBlog} />
-                  <Route exact path="/blogs" component={ListBlog} />
-                  <Route exact path="/blog/create" component={CreateBlog} />
-                  <Route exact path="/blog/:id" component={ShowBlog} />
-                  <Route exact path="/" component={Resume} />
-                </Switch>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <BackTop>
+                        <Icon type="up-circle" />
+                    </BackTop>
+                    <Row>
+                        <Col lg={12}>
+                            <Intro />
+                        </Col>
+                        <Col lg={12}>
+                            <div>
+                                <Switch>
+                                    <Route
+                                        exact
+                                        path="/login"
+                                        component={Login}
+                                    />
+                                    <Route
+                                        exact
+                                        path="/blogs/:page"
+                                        component={ListBlog}
+                                    />
+                                    <Route
+                                        exact
+                                        path="/blogs"
+                                        component={ListBlog}
+                                    />
+                                    <Route
+                                        exact
+                                        path="/blog/create"
+                                        component={CreateBlog}
+                                    />
+                                    <Route
+                                        exact
+                                        path="/blog/:id"
+                                        component={ShowBlog}
+                                    />
+                                    <Route exact path="/" component={Resume} />
+                                </Switch>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
