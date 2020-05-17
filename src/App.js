36cc2722643fec440a8ab784c1eb8fb5d90d9421
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-05-17 16:06:30 +0700

    add material design icons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import avatar from './avatar.jpg';
+import Icon from '@mdi/react';
+import { mdiAccount } from '@mdi/js';
 
 function App() {
   return (
     <Container fluid>
       <Row>
-        <Col md={12} lg={5} xl={6} className="left-col">
+        <Col md={12} lg={4} xl={5} className="left-col">
           <div>
             <Image
               roundedCircle
@@ -18,9 +20,17 @@ function App() {
             />
             <h1 className="name">Huynh Duc Duy</h1>
             <h2 className="title">Web Developer</h2>
+            <Icon
+              path={mdiAccount}
+              size={1}
+              horizontal
+              vertical
+              rotate={90}
+              color="red"
+            />
           </div>
         </Col>
-        <Col md={12} lg={7} xl={6} className="right-col">
+        <Col md={12} lg={8} xl={7} className="right-col">
           <div style={{ height: 2000 }}>ahihi</div>
         </Col>
       </Row>
