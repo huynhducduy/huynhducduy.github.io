735dbec4742cae8d16175ae29483161b52ab4168
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-05-17 15:33:05 +0700

    left col

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import './App.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import './App.scss';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import avatar from './avatar.jpg';
 
 function App() {
   return (
     <Container fluid>
       <Row>
-        <div className="left-col">1 of 2</div>
-        <div className="right-col">
+        <Col md={12} lg={5} xl={6} className="left-col">
+          <div>
+            <Image
+              roundedCircle
+              src={avatar}
+              alt="avatar"
+              width={250}
+              className="avatar"
+            />
+            <h1 className="name">Huynh Duc Duy</h1>
+            <h2 className="title">Web Developer</h2>
+          </div>
+        </Col>
+        <Col md={12} lg={7} xl={6} className="right-col">
           <div style={{ height: 2000 }}>ahihi</div>
-        </div>
+        </Col>
       </Row>
     </Container>
   );
