d50cb2a1028910eb50f85a73d5f8628b6a1b8cad
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-01-09 18:16:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import './App.scss';
-import { Container, Row, Col } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import Resume from 'app/resume';
-import Info from 'app/info';
-import Blog from 'app/blog';
+import Header from 'components/Header';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/blog">
-          <Blog />
-        </Route>
         <Route path="/">
-          <Container fluid>
-            <Row>
-              <Info />
-              <Col md={12} lg={8} xl={7} className="right-col">
-                <Resume />
-              </Col>
-            </Row>
-          </Container>
+          <Header />
         </Route>
       </Switch>
     </Router>
