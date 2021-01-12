184c50b5431095f293499ce2c94e7207e4feb240
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-01-12 23:53:38 +0700

    gohugo

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2021-01-12T23:49:08+07:00
+draft: true
+---
+
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  name: process.env.REACT_APP_NAME,
-  api: process.env.REACT_APP_API,
-};
