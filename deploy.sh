02fce11abf2e696ac88c62944704f27c998caec2
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-01-16 04:53:34 +0700

    update

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+hugo
+cd public
+git add -A
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+git push origin master
