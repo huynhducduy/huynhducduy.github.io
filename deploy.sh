72d3231a23aca32196cde7c47d196ace2845fecc
Huynh Duc Duy <duy.huynh@linecorp.com>
2022-06-16 15:12:20 +0700

    Rebuild blog

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,12 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 hugo
 cd public
+git init
+git remote add origin git@github.com:huynhducduy/huynhducduy.github.io.git
 git add -A
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
-git push origin master
+git push -f origin main
